69b64f6568ca6fde9feaefa667e06358a4773512
CleanCodeCreature <lambertlee410@gmail.com>
2019-12-20 16:43:14 +0530

    data collection,data cleaning,data visualization complete

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,34 @@
+//create a separate file for cloring in rectangle
+var jsonData = {
+  entries: []
+}
+document.getElementById("ReColorBgButton").style.display = "none";
+const colorByLabel = {
+
+  "Green-ish": [],
+  "Blue-ish": [],
+  'Red-ish': [],
+  "Yellow-ish": [],
+  "Pink-ish": [],
+  "Brown-ish": [],
+  "Gray-ish": [],
+  "Purple-ish": []
+}
+
 var r, g, b;
 var database;
 const colorBG = () => {
-  createCanvas(840, 480).parent('SketchHolder');
+
+  const visible = document.getElementById("ReColorBgButton").style.display = "none";
+
+
+  createCanvas(400, 340).parent('SketchHolder');
   r = Math.floor(Math.random() * 256);
   g = Math.floor(Math.random() * 256);
   b = Math.floor(Math.random() * 256);
   background(r, g, b);
+  document.getElementById('SketchHolder').style.backgroundColor = "rgba(" + r + "," + g + "," + b + "," + "0.8)";
+
 }
 
 function setup() {
@@ -28,6 +51,10 @@ function setup() {
 
 }
 //function for the buttons
+
+//this will be a multimethods button which will fire giveColor() function
+//if it works
+
 const giveColor = (id) => {
   const colorData = document.getElementById(id).innerText;
 
@@ -50,4 +77,63 @@ const giveColor = (id) => {
   //color the canvas again
   colorBG();
 
+}
+
+const getData = () => {
+  database.collection("colors-storage").get().then(
+
+    (querySnapshot) => {
+      //console.log(querySnapshot.docs);
+
+      let setOfData = querySnapshot.docs;
+
+      //convert all keys of object to array
+      keys = Object.keys(setOfData);
+      //console.log(keys.length);
+
+      for (key in keys) {
+        //const id=setOfData[key].id;
+        const data = setOfData[key].data();
+
+        //store all the datas in a object for downloading
+        jsonData.entries.push(data);
+
+        const dataLabel = data.label;
+        //console.log(dataLabel);
+
+        let colorBox = color(data.r, data.g, data.b);
+        colorByLabel[dataLabel].push(colorBox);
+      }
+      //console.log(colorByLabel);
+      //console.log(querySnapshot);
+    });
+}
+
+
+var x = 0,
+  y = 0,
+  ish = [];
+
+//Visualize in shape of rectangle
+const colorData = () => {
+  document.getElementById('ReColorBgButton').style.display = "block";
+  createCanvas(400, 400).parent('Colors-holder');
+
+  ish = colorByLabel['Gray-ish'];
+
+  for (let i = 0; i < ish.length; i++) {
+    rect(x, y, 40, 40);
+    fill(ish[i]);
+    noStroke();
+    x += 40;
+    if (x > width) {
+      x = 0;
+      y += 40;
+    }
+  }
+}
+
+//Downlaod the data (ColorByLable object)
+var downloadData = () => {
+  saveJSON(jsonData, 'colorData.json');
 }
